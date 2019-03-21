e92fc673d58c061a9e8c54e2df4ba4ce5fd09950
Yasuyuki <yas.la.pedrera78@gmail.com>
2019-03-21 15:14:18 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import _ from 'lodash';
 
+const storedNum = [];
+let i = 0;
+
 function addRandomNum() {
-  const mainEl = document.getElementById('main');
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
-  document.body.appendChild(p)
+  let p = document.createElement('p');
+  const randomNum = _.random(0, 10);
+  p.innerHTML = "Random Number: " + randomNum.toString();
+  document.body.appendChild(p);
+  const recordNumber = () => storedNum.push(randomNum);
+  if (i < 5) {
+    recordNumber();
+    i++;
+  }
 }
 
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
   console.log("ready");
+}
+
+function sumNum() {
+  let p = document.createElement('p');
+  p.innerHTML = "Sum: " + _.sum(storedNum).toString();
+  document.body.appendChild(p);
+}
+
+{
+  const button2 = document.getElementById('button2')
+  button2.addEventListener("click", sumNum);
 }
